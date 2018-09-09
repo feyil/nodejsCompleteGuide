98988e4216ae5b584f78d8431db020af93cab327
Furkan Emre <furkan54emre@gmail.com>
2018-09-09 14:58:30 +0300

    Event Arguments

--- eventsModule/app.js
+++ eventsModule/app.js
@@ -6,10 +6,22 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on("messageLogged", function() {
-    console.log("Listener called");
+emitter.on("messageLogged", function(arg) { // e, eventArg can be used
+    console.log("Listener called;" , arg);
 });
 
 
 // Raise an event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://"}); //Event Arguments
+
+const logingEmitter = new EventEmitter();
+
+// Register a listener
+
+logingEmitter.on("logging", (message) => {
+    console.log("Loging Listener called:", message);
+});
+
+// Raise an event
+
+logingEmitter.emit("logging", "Furkan Emre");
