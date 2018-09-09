4710cd5ed6c7c7936a813d7cfae0aeabc0c54652
Furkan Emre <furkan54emre@gmail.com>
2018-09-09 13:35:03 +0300

    Quick look the module system

--- first-app/app.js
+++ first-app/app.js
@@ -1,6 +1,11 @@
 function sayHello(name) {
-    console.log("Hello " + name);
+    console.log("Hello " + name); //global
 }
 
 sayHello("Furkan Emre");
-//console.log(window) // in node we dont have window and document object
+//console.log(window) // in node we dont have window and document objects
+
+var message = ''; // They are not binded to the global scope only that file
+console.log(global.message);
+
+console.log(module)
