3403d52d44c73201a50ce20eade9c96a74d06d0a
Furkan Emre <furkan54emre@gmail.com>
2018-09-10 16:51:00 +0300

    Basic input validation

--- express-demo/index.js
+++ express-demo/index.js
@@ -33,6 +33,11 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        // 400 Bad Request
+        res.status(400).send("Name is required and shoudl be minimum 3 char");
+        return;
+    }
     const course = {
         id: courses.length + 1,
         name: req.body.name
