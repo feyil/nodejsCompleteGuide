c8ee294fc546a82ba6dbcecaf896f3e0e68ca6c1
Furkan Emre <furkan54emre@gmail.com>
2018-09-10 16:30:34 +0300

    Handling HTTP GET request

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,17 +9,25 @@ const app = express();
  app.put()
  app.delete()
 */
+const courses = [
+    { id:1, name: "course1"},
+    { id:2, name: "course2"},
+    { id:3, name: "course3"}
+];
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.get("/api/courses", (req, res) => {
-    res.send([1, 2, 3, 4, 5]);
+    res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    // we can use let if we want to reset the value later
+    if (!course) res.status(404).send("The course with the given ıd was not found");
+    res.send(course);
 });
 
 app.get("/api/posts/:year/:month", (req, res) => {
