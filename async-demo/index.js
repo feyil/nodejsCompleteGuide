9de6bc711a1825c589fcd476410af10bba3c7414
Furkan Emre <furkan54emre@gmail.com>
2018-09-11 15:03:13 +0300

    Simple Callback Example

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,7 +1,9 @@
 
 console.log("Before");
 
-const user = getUser(1); // user is undefined
+getUser(1, (user) => {
+    console.log("User",user);
+});
 
 // Callbacks
 // Promises
@@ -9,10 +11,16 @@ const user = getUser(1); // user is undefined
 
 console.log("After");
 
-function getUser(id) {
+function getUser(id, callback) {
     setTimeout(() => {
         console.log("Reading a user from a database...");
-        return { id: id, gitHubUsername: "furkan"};
+        callback({ id: id, gitHubUsername: "furkan"});
     }, 2000);
 }
 
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log("Calling GitHub API...");
+        callback(["repo1", "repo2", "repo3"]);
+    }, 2000);
+}
