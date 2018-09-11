1e4867f79cb61779bc40191cc53458da08acaafc
Furkan Emre <furkan54emre@gmail.com>
2018-09-11 15:45:28 +0300

    Improving understanding of promises

--- async-demo/promise.js
+++ async-demo/promise.js
@@ -2,10 +2,19 @@
 const p = new Promise((resolve, reject) => {
     // Kick off some async work
     // ...
-    // resolve(1);
-    // reject(new Error("message"));
+    // resolve(1); // pending => resolved, fulfilled
+    // reject(new Error("message")); // pending => rejected
 
-    resolve(1);
+    setTimeout(() => {
+        //resolve(1);
+        reject(new Error("message"));
+    }, 2000);
 });
 
-p.then(result => console.log("Result", result));
+console.log("Before");
+
+p
+    .then(result => console.log("Result", result))
+    .catch(err => console.log("Error", err.message));
+
+console.log("After");
