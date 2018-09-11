fac04948145d4ed6c33bf4f9c8a190c96c978d48
Furkan Emre <furkan54emre@gmail.com>
2018-09-12 00:41:09 +0300

    Quick look to regular expressions

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -40,12 +40,26 @@ async function getCourses() {
     // in
     // nin (not in)
 
+    // or
+    // and
+
 
     // const courses = await Course.find(); // Listing all items
     const courses = await Course
-                  //          .find({ author:"Mosh", isPublished: true})
+                           // .find({ author:"Mosh", isPublished: true})
                            // .find({price: { $gte: 10, $lte: 20}})
-                            .find({price: { $in: [10, 50, 20]}})
+                           // .find({price: { $in: [10, 50, 20]}})
+                           // .find()
+                           // .or([{author: "Mosh"}, { isPublished: true}])
+
+                           // Starts with Mosh
+                            .find({ author: /^Mosh/ })
+
+                           // Ends with Hamedani
+                            .find({author: /Hamedani$/i }) // i for case insensitive
+
+                          // Contains Mosh
+                            .find({ author: /.*Mosh.*/i})
                             .limit(10)
                             .sort({name:1})
                             .select({name: 1, tags: 1});
