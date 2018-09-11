90d1da249043fb67e5d3e85e98cde21bbf9767d6
Furkan Emre <furkan54emre@gmail.com>
2018-09-11 17:23:47 +0300

    Running Parallel Promises

--- replacing_promises/promise-api.js
+++ replacing_promises/promise-api.js
@@ -1,5 +1,24 @@
 
-const p = Promise.resolve({id:1});
-const p1 = Promise.reject(new Error("reason for rejection")); // include call stack
-p.then(result => console.log(result));
-p1.catch(error => console.log(error));
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async operation 1...");
+        resolve(1);
+      // reject(new Error("because something failed"));
+    },2000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async operation 2...");
+        resolve(2);
+    },2000);
+})
+
+Promise.all([p1, p2])
+    .then(result => console.log(result))
+    .catch(error => console.log(error)); // If one of them rejected all consider as rejected
+
+Promise.race([p1, p2])
+    .then(result => console.log(result))
+    .catch(error => console.log(error)); // If one of them rejected all consider as rejected
+ 
