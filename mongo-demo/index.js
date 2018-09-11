451f30aad27988bf3bbbf61d36182a1dd0363374
Furkan Emre <furkan54emre@gmail.com>
2018-09-12 00:32:27 +0300

    Logical Query Operators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -31,9 +31,21 @@ async function createCourse() {
 }
 
 async function getCourses() {
+    // eq (equal)
+    // ne (not equal)
+    // gt (greater than)
+    // gte (greater than or equal to)
+    // lt (less than)
+    // lte (less than or equal to)
+    // in
+    // nin (not in)
+
+
     // const courses = await Course.find(); // Listing all items
     const courses = await Course
-                            .find({ author:"Mosh", isPublished: true})
+                  //          .find({ author:"Mosh", isPublished: true})
+                           // .find({price: { $gte: 10, $lte: 20}})
+                            .find({price: { $in: [10, 50, 20]}})
                             .limit(10)
                             .sort({name:1})
                             .select({name: 1, tags: 1});
