5aa287b892c39f267d7fad5c3f17fd5567372586
Furkan Emre <furkan54emre@gmail.com>
2018-09-12 18:41:10 +0300

    Custom validators

--- validation/index.js
+++ validation/index.js
@@ -19,7 +19,15 @@ const courseSchema = new mongoose.Schema({
         enum: ["web", "mobile", "network"]
     },
     author: String,
-    tags: [String],
+    tags: { // Custom validator
+        type: Array,
+        validate: {
+            validator: function(v) {
+                return v.length > 0;
+            },
+            message: "A course should have at least one tag."
+        }
+    },
     date: { type: Date, default: Date.now},
     isPublished: Boolean,
     price :{
@@ -35,9 +43,9 @@ const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
     const course = new Course({
         name: "Angular Course",
-        category: "-",
+        category: "web",
         author: "Furkan",
-        tags: ["angular", "frontend"],
+        tags: [],
         isPublished: true,
         price: 15
     });
