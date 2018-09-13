fe28460a1b054ca5b7c0a7bf8620fee23fb90014
Furkan Emre <furkan54emre@gmail.com>
2018-09-13 16:21:41 +0300

    Using array of sub documents

--- Referencing Documents/embedding.js
+++ Referencing Documents/embedding.js
@@ -14,16 +14,16 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: {
-    type: authorSchema,
+  authors: {
+    type: [authorSchema],
     required: true
   }
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -51,9 +51,27 @@ async function updateAuthor(courseId) {
     }
   });
   */
-
-
 } 
 
-// createCourse('Node Course', new Author({ name: 'Mosh' }));
-updateAuthor("5b9a4748ce5b803c111772b4")
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+}
+
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  author.remove();
+  course.save();
+}
+
+//createCourse('Node Course', [
+//  new Author({ name: 'Mosh' }),
+//  new Author({ name: "Furkan"})]);
+
+//updateAuthor("5b9a4748ce5b803c111772b4")
+
+// addAuthor("5b9a4bbff512bd41479d0458", new Author({ name: "Ammy"}));
+
+removeAuthor("5b9a4bbff512bd41479d0458", "5b9a4cae8236b7425375b006");
