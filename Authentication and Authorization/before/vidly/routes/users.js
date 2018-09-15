ee61aa36056ca85fcabf495b14710eeb3e2a720c
Furkan Emre <furkan54emre@gmail.com>
2018-09-15 15:59:49 +0300

    Getting the current user

--- Authentication and Authorization/before/vidly/routes/users.js
+++ Authentication and Authorization/before/vidly/routes/users.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const bcrypt = require("bcrypt");
@@ -7,6 +8,12 @@ const mongoose = require("mongoose");
 const express = require("express");
 
 const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
+    res.send(user);
+
+}); 
 
 router.post("/", async (req, res) => {
     const { error} = validate(req.body);
