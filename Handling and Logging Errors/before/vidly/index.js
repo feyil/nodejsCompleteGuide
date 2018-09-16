3f0cdd0ed7e313fe388f96127e5f910d24a12f01
Furkan Emre <furkan54emre@gmail.com>
2018-09-16 16:54:41 +0300

    Refactoring the code

--- Handling and Logging Errors/before/vidly/index.js
+++ Handling and Logging Errors/before/vidly/index.js
@@ -1,49 +1,12 @@
-require("express-async-errors");
-const winston = require("winston");
-require("winston-mongodb");
-
-const config = require('config');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-
 
 const express = require('express');
 const app = express();
+
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-process.on("uncaughtException", (ex) => {
-  console.log("We got an uncaught exception.");
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-process.on("unhandledRejection", (ex) => {
-  console.log("We got an unhandled rejection exception.");
-  winston.error(ex.message, ex);
-  process.exit(1);
-
-  // throw ex;
-});
-
-// winston.handleExceptions(new winston.transpots.File({filename: "uncaughtExceptions.log" }))
-
-
-winston.add(new winston.transports.File({ filename: "logfile.log" })); 
-winston.add(new winston.transports.Console());
-winston.add(new winston.transports.MongoDB({db: "mongodb://localhost/vidly"}));
-
-// const p = Promise.reject(new Error("Something failed miserably!"));
-// p.then(() => console.log("Done"));
-
-
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
-}
-
-
-
+require("./startup/config")();
+require("./startup/validation")();
 
 
 const port = process.env.PORT || 3000;
