29cebca76d85eb9894a6dcfa8d0159adcbf440e4
Furkan Emre <furkan54emre@gmail.com>
2018-09-16 14:23:19 +0300

    Logging Errors

--- Handling and Logging Errors/before/vidly/routes/genres.js
+++ Handling and Logging Errors/before/vidly/routes/genres.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 
 router.get('/', async (req, res) => {
+  throw new Error("Could no t get the genres");
   const genres = await Genre.find().sort("name");
   res.send(genres);
 });
