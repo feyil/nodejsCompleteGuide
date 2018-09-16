a9d5e3ea18ad027a9d8b32392606788d58f2b96a
Furkan Emre <furkan54emre@gmail.com>
2018-09-16 15:15:24 +0300

    Unhandled Promise Rejection

--- Handling and Logging Errors/before/vidly/index.js
+++ Handling and Logging Errors/before/vidly/index.js
@@ -18,12 +18,26 @@ const app = express();
 process.on("uncaughtException", (ex) => {
   console.log("We got an uncaught exception.");
   winston.error(ex.message, ex);
+  process.exit(1);
 });
+
+process.on("unhandledRejection", (ex) => {
+  console.log("We got an unhandled rejection exception.");
+  winston.error(ex.message, ex);
+  process.exit(1);
+
+  // throw ex;
+});
+
+// winston.handleExceptions(new winston.transpots.File({filename: "uncaughtExceptions.log" }))
 
 
 winston.add(new winston.transports.File({ filename: "logfile.log" })); 
 winston.add(new winston.transports.Console());
 winston.add(new winston.transports.MongoDB({db: "mongodb://localhost/vidly"}));
+
+const p = Promise.reject(new Error("Something failed miserably!"));
+p.then(() => console.log("Done"));
 
 
 if (!config.get('jwtPrivateKey')) {
