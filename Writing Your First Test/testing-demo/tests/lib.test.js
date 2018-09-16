6c8001b25b964310db1eeee99fc55c2612c8a30b
Furkan Emre <furkan54emre@gmail.com>
2018-09-16 22:59:29 +0300

    Testing Numbers

--- Writing Your First Test/testing-demo/tests/lib.test.js
+++ Writing Your First Test/testing-demo/tests/lib.test.js
@@ -1,4 +1,18 @@
+const lib = require("../lib");
 
-test("Our first test", () => {
-    throw new Error("Something failed");
+test("absolute - should return a positive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
 });
+
+test("absolute - should return a positive number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+
+test("absolute - should return 0 if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+});
+
+// Matcher functions documentation
